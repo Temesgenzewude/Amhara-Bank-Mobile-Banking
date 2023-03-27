9327c6758d63d2b182f12d6c32473200ebdb3ede
abi26anamo <abianamo282@gmail.com>
2023-03-27 15:10:09 +0300

    added login_pages

---
+++ lib/presentation/login_pages/login_button.dart
@@ -0,0 +1,27 @@
+import 'package:amhara_bank_mobile_banking/presentation/login_pages/pin_setup_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  final String routeName;
+
+  const LoginButton({super.key, required this.routeName});
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    return SizedBox(
+      height: screenSize.height * 0.06,
+      width: screenSize.width * 0.4,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, routeName);
+        },
+        style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF005EA6),
+            textStyle:
+                const TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+        child: const Text('Login'),
+      ),
+    );
+  }
+}
