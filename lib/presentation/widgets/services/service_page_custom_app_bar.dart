59b048f207c4c2da0576082f269d6263b8eb5381
Temesgen <temesgenzewude21@gmail.com>
2023-03-30 14:10:34 +0300

    added fonts and updated dimensions on services custome app bar

--- lib/presentation/widgets/services/service_page_custom_app_bar.dart
+++ lib/presentation/widgets/services/service_page_custom_app_bar.dart
@@ -13,11 +13,11 @@ class ServicePageCustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
       margin: EdgeInsets.only(
-          left: AppDimension.width20,
-          right: AppDimension.width20,
-          top: AppDimension.height30),
+          left: AppDimension.width20 + AppDimension.width4,
+          right: AppDimension.width20 + AppDimension.width4,
+          top: AppDimension.height10*4),
+
       color: Colors.white,
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         appBarLeftSideWidget,
@@ -38,33 +38,34 @@ class ServicePageCustomAppBar extends StatelessWidget {
               children: [
                 Icon(
                   Icons.notifications_outlined,
+                  
                   color: const Color(
                     0xFF143B58,
                   ),
                   size: AppDimension.iconSize30,
                 ),
-                Positioned(
-                  top: AppDimension.height10 - 1,
-                  right: AppDimension.width5,
-                  child: Container(
-                    height: AppDimension.height15,
-                    width: AppDimension.width15,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFC90101),
-                      borderRadius:
-                          BorderRadius.circular(AppDimension.radius15 / 2),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "2",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: AppDimension.font10,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ),
+                // Positioned(
+                //   top: AppDimension.height10 - 1,
+                //   right: AppDimension.width5,
+                //   child: Container(
+                //     height: AppDimension.height15,
+                //     width: AppDimension.width15,
+                //     decoration: BoxDecoration(
+                //       color: const Color(0xFFC90101),
+                //       borderRadius:
+                //           BorderRadius.circular(AppDimension.radius15 / 2),
+                //     ),
+                //     child: Center(
+                //       child: Text(
+                //         "2",
+                //         style: TextStyle(
+                //             color: Colors.white,
+                //             fontSize: AppDimension.font10,
+                //             fontWeight: FontWeight.bold),
+                //       ),
+                //     ),
+                //   ),
+                // ),
               ],
             )
           ],
