3364f02aea570a8b06713c684714ffe8a8b33aa7
abi26anamo <abianamo282@gmail.com>
2023-04-01 22:48:22 +0300

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import 'package:amhara_bank_mobile_banking/presentation/login_pages/activation_page.dart';
 import 'package:amhara_bank_mobile_banking/presentation/login_pages/login_page.dart';
 import 'package:amhara_bank_mobile_banking/presentation/login_pages/pin_setup_page.dart';
 import 'package:amhara_bank_mobile_banking/presentation/services/services_home_page.dart';
-=======
 
->>>>>>> 2964d5b162dab24e378369754eb27bf33c6b1d68
 import 'package:amhara_bank_mobile_banking/presentation/splashscreen.dart';
 import 'package:amhara_bank_mobile_banking/presentation/transfer_pages/own_account_transfer/own_account_transfer.dart';
 import 'package:amhara_bank_mobile_banking/presentation/transfer_pages/successfully_transfered.dart';
